b80bc59107a79d3be0e92f006977e345a093750d
Soya Beans <5205954@student.gloscol.ac.uk>
2020-01-17 10:33:46 +0000

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function welcomeMessage(){
+  alert('Hello World program - there always is a hello world program!');
+}
+
+function nameMessage(){
+  var item=document.getElementById("name");
+
+  alert("Hello" + " " + item.value);
+}
